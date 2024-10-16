1a28c920215eb4c3b6bab560b12e12859a6f0d97
Rauleeeto <141586367+Rauleeeto@users.noreply.github.com>
2024-10-16 13:53:23 -0400

    Update routers.js

--- src/routers.js
+++ src/routers.js
@@ -1,8 +1,11 @@
 import SignUp from "./components/SignUp.vue";
 import CancelarTickets from "./components/CancelarTickets.vue";
 import RegistrarTickets from "./components/RegistrarTickets.vue";
+import RegistrarUsuario from "./components/RegistrarUsuario.vue"; 
 import DeliveryTickets from "./components/DeliveryTickets.vue";
-import AssignmentTickets from "./components/AssignmentTickets.vue"; 
+import AssignmentTickets from "./components/AssignmentTickets.vue";
+import PerfilUsuario from "./components/PerfilUsuario.vue";
+import RecuperarContraseña from "./components/RecuperarContraseña.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
@@ -30,7 +33,22 @@ const routes = [
     name: "AssignmentTickets",  
     component: AssignmentTickets,
     path: "/assignment-tickets"  
-  }
+  },
+   {
+    name: "PerfilUsuario",
+    component: PerfilUsuario,
+    path: "/perfil-usuario",
+  },
+  {
+    name: "RegistrarUsuario",
+    component: RegistrarUsuario,
+    path: "/registrar-usuario",
+  },
+    {
+    name: "RecuperarContraseña",
+    component: RecuperarContraseña, 
+    path: "/recuperar-contraseña",  
+  },
 ];
 
 const router = createRouter({
