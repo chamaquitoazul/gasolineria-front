20acb357743804953c976e013c181e75a5fdd7b3
Anti <110852644+ADRDevp@users.noreply.github.com>
2024-10-16 12:56:21 -0400

    Add files via upload

--- src/routers.js
+++ src/routers.js
@@ -1,6 +1,8 @@
 import SignUp from "./components/SignUp.vue";
 import CancelarTickets from "./components/CancelarTickets.vue";
 import RegistrarTickets from "./components/RegistrarTickets.vue";
+import DeliveryTickets from "./components/DeliveryTickets.vue";
+import AssignmentTickets from "./components/AssignmentTickets.vue"; 
 import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
@@ -9,19 +11,26 @@ const routes = [
     component: SignUp,
     path: "/",
   },
-
   {
     name: "CancelarTickets",
     component: CancelarTickets,
     path: "/cancelar-tickets",
   },
-  
   {
     name: "RegistrarTickets",
     component: RegistrarTickets,
-    path:"/registrar-tickets"
+    path: "/registrar-tickets"
   },
-
+  {
+    name: "DeliveryTickets",
+    component: DeliveryTickets, 
+    path: "/delivery-tickets"
+  },
+  {
+    name: "AssignmentTickets",  
+    component: AssignmentTickets,
+    path: "/assignment-tickets"  
+  }
 ];
 
 const router = createRouter({
